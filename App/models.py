522081d92db1b9b54de6d25a2de0a45119aa7d88
Nicholas Mendez <snickdx@gmail.com>
2024-04-09 04:31:27 +0000

    first commit

---
+++ App/models.py
@@ -0,0 +1,35 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(80), unique=True, nullable=False)
+  password = db.Column(db.String(120), nullable=False)
+
+  def __init__(self, username, password):
+    self.username= username
+    self.set_password(password)
+
+  def set_password(self, password):
+    self.password = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.password, password)
+    
+class Student(db.Model):
+  id = db.Column(db.String(9), primary_key=True)
+  # courses = db.relationship('Course', secondary='student_course', backref='students')
+
+
+  
+class Course(db.Model):
+  code = db.Column(db.String(9), primary_key=True)
+
+
+class StudentCourse(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+
+
